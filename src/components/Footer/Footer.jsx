90596ff8989afb890eda176a0e3b3381f8a137ea
Nurbolot <nubolotkadyrov06@gmail.com>
2024-05-02 23:31:52 +0600

    added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
-
+import styles from '../../styles/Footer.module.css';
+import { ROUTES } from '../../utils/routes';
+import LOGO from '../../images/logo.svg';
+import { Link } from 'react-router-dom';
+import { CiInstagram } from "react-icons/ci";
+import { CiFacebook } from "react-icons/ci";
+import { CiYoutube } from "react-icons/ci";
 const Footer = () => {
     return (
-        <div>
+        <section className={styles.footer}>
+            <div className={styles.logo}>
+                <Link to={ROUTES.HOME}>
+                    <img src={LOGO} alt="Stuff" />
+                </Link>
+            </div>
+            <div className={styles.rights}>
+                Developed by <a href="#">Nu1bo</a>
+            </div>
+            <div className={styles.socials}>
+                <a href="#">
+                    <div className={"icon"}>
+                        <CiYoutube/>
+                    </div>
+                </a>
 
-        </div>
+                <a href="#">
+                    <div className={"icon"}>
+                        <CiFacebook/>
+                    </div>
+                </a>
+
+                <a href="#">
+                    <div className={"icon"}>
+                        <CiInstagram/>
+                    </div>
+                </a>
+            </div>
+        </section>
     )
 }
 
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import styles from '../../styles/Header.module.css'
+import styles from '../../styles/Sidebar.module.css'
 import { Link } from 'react-router-dom'
 import { ROUTES } from '../../utils/routes'
 const Sidebar = () => {
   return (
-    <div>
-      Footer
+    <div className={styles.sidebar}>
+      
     </div>
   )
 }
